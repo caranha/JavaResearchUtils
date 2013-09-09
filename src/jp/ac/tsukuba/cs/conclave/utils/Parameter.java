f2b473bbdf94e93c7a93ae2087ed68bcd818b9be
Claus Aranha <caranha@cs.tsukuba.ac.jp>
2013-09-09 13:30:20 +0900

    Initial Commit

---
+++ src/jp/ac/tsukuba/cs/conclave/utils/Parameter.java
@@ -0,0 +1,21 @@
+package jp.ac.tsukuba.cs.conclave.utils;
+
+/**
+ * This class holds a set of parameters, that can be used for a variety of projects.
+ * 
+ * Parameters are stored as values associated with unique key strings. Parameter values are 
+ * stored and retrieved as strings. Key values are NOT case sensitive.
+ * 
+ * These parameters can be read from a text file with the following sintax:
+ * * Lines starting with "#" are commentary, and are ignored.
+ * * Any text past a # is treated as a commentary, and ignored.
+ * * Parameters are on the form: "A = B", where A and B can be any string of characters.
+ * 
+ * 
+ * @author caranha
+ *
+ */
+
+public class Parameter {
+
+}
